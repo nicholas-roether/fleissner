4b8e941a211e19bed74cbfc50ba0d1a47a078941
nicholas-roether <nicholas.roether@t-online.de>
2021-02-12 14:40:15 +0100

    Add debug grille to visualization page

--- src/pages/visualization/visualization.tsx
+++ src/pages/visualization/visualization.tsx
@@ -1,6 +1,27 @@
-import { Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core";
 import React from "react";
+import { FleissnerGrille } from "./grille";
+import LetterGrid from "./letter-grid";
 
-const VisualizationPage: React.FC = () => <Typography>Visualization</Typography>;
+const useStyles = makeStyles({
+	container: {
+		position: "relative",
+		maxWidth: "600px"
+	},
+	content: {
+		position: "absolute",
+		width: "100%",
+	}
+});
+
+const VisualizationPage: React.FC = () => {
+	const classes = useStyles();
+	return (
+		<div className={classes.container}>
+			<div className={classes.content}><LetterGrid size={6} letters={["a", "b", "c", "d", "e", "f"]} fontSize="7em" /></div>
+			<div className={classes.content}><FleissnerGrille /></div>
+		</div>
+	);
+};
 
 export default VisualizationPage;
