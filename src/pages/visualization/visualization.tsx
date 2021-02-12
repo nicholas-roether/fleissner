d36482e83cab155f76457e43a7594bafe95ca48c
nicholas-roether <nicholas.roether@t-online.de>
2021-02-12 16:59:57 +0100

    temporarily center fleissner widget

--- src/pages/visualization/visualization.tsx
+++ src/pages/visualization/visualization.tsx
@@ -1,4 +1,4 @@
-import { Button } from "@material-ui/core";
+import { Box, Button } from "@material-ui/core";
 import React from "react";
 import FleissnerGrilleWidget from "./fleissner-grille-widget";
 
@@ -8,10 +8,14 @@ const defaultMessage = "Moin, dies ist die Fleißnersche Schablone"
 const VisualizationPage: React.FC = () => {
 	const [rotation, setRotation] = React.useState<number>(0);
 	return (
-		<>
-			<FleissnerGrilleWidget message={defaultMessage} rotation={rotation} />
-			<Button variant="contained" onClick={() => setRotation(rotation + 1)}>Rotate</Button>
-		</>
+		<Box display="flex" flexDirection="column" alignItems="center">
+			<Box width="600px">
+				<FleissnerGrilleWidget message={defaultMessage} rotation={rotation} />
+			</Box>
+			<span>
+				<Button variant="contained" onClick={() => setRotation(rotation + 1)}>Rotate</Button>
+			</span>
+		</Box>
 	);
 };
 
