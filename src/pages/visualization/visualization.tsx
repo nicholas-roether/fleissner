f9a3282edf8a8b206afbc26f500df215a9d4600f
nicholas-roether <nicholas.roether@t-online.de>
2021-02-12 14:57:07 +0100

    Add default message

--- src/pages/visualization/visualization.tsx
+++ src/pages/visualization/visualization.tsx
@@ -1,5 +1,6 @@
 import { makeStyles } from "@material-ui/core";
 import React from "react";
+import { fleissnerEncode } from "../../utils/fleissner-utils";
 import { FleissnerGrille } from "./grille";
 import LetterGrid from "./letter-grid";
 
@@ -14,11 +15,16 @@ const useStyles = makeStyles({
 	}
 });
 
+const defaultMessage = "Moin, dies ist die Fleißnersche Schablone"
+
 const VisualizationPage: React.FC = () => {
 	const classes = useStyles();
+	const messageSanitized = defaultMessage.toUpperCase().split("").filter(char => /[A-Z]/.test(char)).join("");
+	console.log(messageSanitized);
+	const gridLetters = fleissnerEncode(messageSanitized);
 	return (
 		<div className={classes.container}>
-			<div className={classes.content}><LetterGrid size={6} letters={["a", "b", "c", "d", "e", "f"]} fontSize="7em" /></div>
+			<div className={classes.content}><LetterGrid size={6} letters={gridLetters} fontSize="7em" /></div>
 			<div className={classes.content}><FleissnerGrille /></div>
 		</div>
 	);
